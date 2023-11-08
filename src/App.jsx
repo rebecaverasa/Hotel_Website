121d5181a2b4f6cbc1cfe7afd61889eafbdc0f43
rebecaverasa <rebecaverasa@gmail.com>
2023-11-07 23:23:21 -0300

    Aplication created with react-vite using tailwind. This will be a hotel website, an updated version of the repository html/css site hotel

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Entrance from "./components/Entrance";
+
+function App() {
+  return (
+    <div>
+      <Entrance />
+    </div>
+  );
+}
+
+export default App
---
+++ src/components/Entrance.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Entrance = () => {
+    return (
+        <div></div>
+    )
+}
+
+export default Entrance
